6975956c6a96809a92c5db4e6235ac6e796cfa90
smileyharini424-cloud <smileyharini424@gmail.com>
2026-09-18 23:46:42 -0700

    Create tuples.py

---
+++ tuples.py
@@ -0,0 +1,7 @@
+student = ("Harini", 20, "CSE", "Hyderabad")
+
+print("Tuple:", student)
+print("First Element:", student[0])
+print("Last Element:", student[-1])
+print("Sliced Tuple:", student[:3])
+print("Length:", len(student))
